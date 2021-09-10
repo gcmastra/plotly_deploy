a892e8957e5972e978e0ceb4e80540f6099a3f17
gcmastra <gcmastra@yahoo.com>
2021-09-10 00:06:33 -0400

    here we go again

---
+++ plots.js
@@ -0,0 +1,50 @@
+// plots.js
+
+// where is the samples.json file location?
+function init() {
+    var selector = d3.select("#selDataset");
+  
+    d3.json("./samples.json").then((data) => {
+      console.log(data);
+      var sampleNames = data.names;
+      sampleNames.forEach((sample) => {
+        selector
+          .append("option")
+          .text(sample)
+          .property("value", sample);
+      });
+    
+    buildMetadata("940")
+
+  })};
+
+// function optionChanged(newSample) {
+//     console.log(newSample);
+//     }
+  
+
+function optionChanged(newSample) {
+    buildMetadata(newSample);
+    // do not forget to restore the next line after all the functions have been defined 
+    // buildCharts(newSample);
+    }
+
+function buildMetadata(sample) {
+    d3.json("samples.json").then((data) => {
+        var metadata = data.metadata;
+        var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+        var result = resultArray[0];
+        var PANEL = d3.select("#sample-metadata");
+      
+        PANEL.html("");
+        PANEL.append("h6").text("ID: " + result.id);
+        PANEL.append("h6").text("ETHNICITY: " + result.ethnicity);
+        PANEL.append("h6").text("GENDER: " + result.gender);
+        PANEL.append("h6").text("AGE: " + result.age);
+        PANEL.append("h6").text("LOCATION: " + result.location);
+        console.log(PANEL.html)
+        });
+    }
+
+init();
+
